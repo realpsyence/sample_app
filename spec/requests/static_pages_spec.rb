42a8bc352ef7e97ac1d7c5017c83c3f01686da17
David Cummings <david.logan.cummings@gmail.com>
2013-01-23 22:00:08 -0500

    Switched to using postgres locally for testing- as per Heroku

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -11,7 +13,7 @@ describe "Static Pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Sample App | Home")
+                                :text => "#{base_title} | Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "Static Pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Help")
+                                :text => "#{base_title} | Help")
     end
   end
 
@@ -33,10 +35,10 @@ describe "Static Pages" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
-    it "should have the title 'About'" do
+    it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Sample App | About Us")
+                                :text => "#{base_title} | About Us")
     end
   end
 
@@ -45,10 +47,10 @@ describe "Static Pages" do
       visit '/static_pages/contact'
       page.should have_selector('h1', :text => 'Contact Us')
     end
-    it "should have the title 'Contact'" do
+    it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
       page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+                                :text => "#{base_title} | Contact Us")
     end
   end
 
